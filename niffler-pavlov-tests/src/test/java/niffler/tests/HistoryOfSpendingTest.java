e70381260ce2bfcc71d44a43790697b26c540e19
Igor Pavlov <igortvk@ya.ru>
2023-02-21 11:07:59 +0300

    [Niffler] HW-1

---
+++ niffler-pavlov-tests/src/test/java/niffler/tests/HistoryOfSpendingTest.java
@@ -0,0 +1,30 @@
+package niffler.tests;
+
+import niffler.BaseTest;
+import niffler.api.spend.dto.SpendDto;
+import niffler.jupiter.spend.WithSpend;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static niffler.models.Category.PRODUCTS;
+
+class HistoryOfSpendingTest extends BaseTest {
+
+    @WithSpend(amount = 2_500.0, username = "ipavlov")
+    private SpendDto restaurantSpend;
+
+    @WithSpend(category = PRODUCTS, amount = 1_000.0, description = "Спасибо Саше Кирееву", username = "ipavlov")
+    private SpendDto productSpend;
+
+    @BeforeEach
+    void login() {
+        landingPage.open()
+                .clickLoginButton();
+        loginPage.login("ipavlov", "12345");
+    }
+    @Test
+    void historyOfSpendingTableContainsSpending() {
+        homePage.getSpendingTable()
+                .checkContainsSpending(restaurantSpend, productSpend);
+    }
+}
